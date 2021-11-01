5efa0c111f7026be6d6e8573bc5240a79e5da89e
jacquesdubois <jacquesadubois@gmail.com>
2021-11-01 13:40:51 -0700

    Added a subtotal section to the CheckoutPage; created a selectTotal Redux getter func

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,11 +1,16 @@
 import Header from "../components/Header";
 import Image from 'next/image';
 import { useSelector } from "react-redux";
-import { selectItems } from "../slices/basketSlice";
+import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
+import Currency from 'react-currency-formatter';
+import { useSession } from 'next-auth/client';
 
 function Checkout() {
     const items = useSelector(selectItems);
+    const total = useSelector(selectTotal);
+
+    const [session] = useSession();
 
     return (
         <div className='bg-gray-100'>
@@ -39,11 +44,23 @@ function Checkout() {
                 </div>
             </div>
 
-
-
             {/* Right Section */}
-            <div>
+            <div className='flex flex-col bg-white p-10 shadow-md'>
+                {items.length > 0 && (
+                    <>
+                        <h2 className='whitespace-nowrap'>Subtotal ({items.length} items):{' '}
+                        <span className='font-bold'>
+                            <Currency quantity={total} />
+                        </span>
+                        </h2>
 
+                        <button className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 cursor-not-allowed'}`}
+                            disabled={!session}
+                        >
+                            {!session ? 'Sign in to Checkout' : 'Proceed to Checkout'}
+                        </button>
+                    </>
+                )}
             </div>
 
           </main>
