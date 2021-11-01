b1d222f1ce26b9bbc34a8cdcdb0d88fce271809c
jacquesdubois <jacquesadubois@gmail.com>
2021-11-01 15:13:01 -0700

    Completed Stripe payments functionality

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -5,12 +5,33 @@ import { selectItems, selectTotal } from "../slices/basketSlice";
 import CheckoutProduct from "../components/CheckoutProduct";
 import Currency from 'react-currency-formatter';
 import { useSession } from 'next-auth/client';
+import { loadStripe } from '@stripe/stripe-js';
+const stripePromise = loadStripe(process.env.stripe_public_key);
+import axios from 'axios';
 
 function Checkout() {
     const items = useSelector(selectItems);
     const total = useSelector(selectTotal);
 
     const [session] = useSession();
+
+    const createCheckoutSession = async () => {
+        const stripe = await stripePromise;
+
+        // Call the Next.js backend to create a checkout session...
+        const checkoutSession = await axios.post('/api/create-checkout-session',
+        {
+            items,
+            email: session.user.email,
+        });
+
+        // Redirect customer to Stripe Checkout
+        const result = await stripe.redirectToCheckout({
+            sessionId: checkoutSession.data.id,
+        });
+
+        if (result.error) alert(result.error.message);
+    };
 
     return (
         <div className='bg-gray-100'>
@@ -55,7 +76,9 @@ function Checkout() {
                         </h2>
 
                         <button className={`button mt-2 ${!session && 'from-gray-300 to-gray-500 border-gray-200 cursor-not-allowed'}`}
+                            role='link'
                             disabled={!session}
+                            onClick={createCheckoutSession}
                         >
                             {!session ? 'Sign in to Checkout' : 'Proceed to Checkout'}
                         </button>
